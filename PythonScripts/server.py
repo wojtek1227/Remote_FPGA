9df3eb71ed5bfd0b1bd0f935fa19162312ecfbe2
Wojciech Caputa <caputa.wojciech@gmail.com>
2018-07-02 19:08:47 +0200

    Added simple client server scripts for testing.

---
+++ PythonScripts/server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname()
+
+port = 9999
+
+server_socket.bind((host, port))
+
+server_socket.listen(5)
+
+while True:
+    client_socket, addr = server_socket.accept()
+
+    print("Got a connection from %s" % str(addr))
+
+    msg = 'Thank you for connecting' + "\r\n"
+    client_socket.send(msg.encode('ascii'))
+    client_socket.close()
